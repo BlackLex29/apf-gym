8f4973bec7beb6ed905471dba48e8b1d579e1288
BlackLex29 <lextermilo@gmail.com>
2025-11-14 01:23:05 +0800

    newlue

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -3,16 +3,16 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSCEMoX5C8MwNfCCZSsDoiE8coY-KpC-U",
-  authDomain: "apf-gym.firebaseapp.com",
-  projectId: "apf-gym",
-  storageBucket: "apf-gym.firebasestorage.app",
-  messagingSenderId: "443201238666",
-  appId: "1:443201238666:web:c3031c3a79d91bed531f73",
-  measurementId: "G-55JYW41KFM"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth(app)
+export const auth = getAuth(app);
